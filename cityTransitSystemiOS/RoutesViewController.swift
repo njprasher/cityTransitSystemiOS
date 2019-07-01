47858b37a4e49852531c86d8d6aa863f455aeaa4
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-30 22:53:07 -0400

    [Neeraj] Created UI of SignUpVC

---
+++ cityTransitSystemiOS/RoutesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RoutesViewController.swift
+//  cityTransitSystemiOS
+//
+//  Created by Neeraj Prasher on 2019-06-30.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class RoutesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    @IBOutlet weak var btnSignUp: UIButton!
+    
+    @IBOutlet weak var RoutesScrollView: UIScrollView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ cityTransitSystemiOS/SignUpViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SignUpViewController.swift
+//  cityTransitSystemiOS
+//
+//  Created by Neeraj Prasher on 2019-06-30.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var txtFirstName: UITextField!
+    
+    @IBOutlet weak var txtLastName: UITextField!
+    
+    @IBOutlet weak var txtDateOfBirth: UITextField!
+    
+    @IBOutlet weak var txtContact: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var txtPasswordAgain: UITextField!
+    
+    @IBOutlet weak var btnSignUp: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
