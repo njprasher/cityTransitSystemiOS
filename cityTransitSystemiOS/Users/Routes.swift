337a9d1b0e5cdcf3a66504c19f28e69a1a5766eb
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-10 06:45:18 -0400

    [Neeraj] Added Properties to the Routes Class

--- cityTransitSystemiOS/Users/Routes.swift
+++ cityTransitSystemiOS/Users/Routes.swift
@@ -19,6 +19,8 @@ class Routes
     var routeId: String
     var routeStart: String
     var routeEnd: String
+    var routeFrequency: Int
+    var routePrice: Float
     var routeColor: RouteColor
     
     init()
@@ -27,15 +29,19 @@ class Routes
         self.routeId = String()
         self.routeStart = String()
         self.routeEnd = String()
+        self.routeFrequency = Int()
+        self.routePrice = Float()
         self.routeColor = RouteColor.white
     }
     
-    init(routeName: String, routeId: String, routeStart: String, routeEnd: String, routeColor: RouteColor)
+    init(routeName: String, routeId: String, routeStart: String, routeEnd: String, routePrice: Float, routeFrequency: Int , routeColor: RouteColor)
     {
         self.routeName = routeName
         self.routeId = routeId
         self.routeStart = routeStart
         self.routeEnd = routeEnd
+        self.routeFrequency = routeFrequency
+        self.routePrice = routePrice
         self.routeColor = routeColor
     }
 }
