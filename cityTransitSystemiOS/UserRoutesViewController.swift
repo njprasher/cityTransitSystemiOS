6998bace18d5a47600d3c2e9c8ef867d2a41301d
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-17 13:01:47 -0400

    [Neeraj] created UserHomeViewController and UserCardsViewController   UIViewController classes

---
+++ cityTransitSystemiOS/UserRoutesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserRoutesViewController.swift
+//  cityTransitSystemiOS
+//
+//  Created by Neeraj Prasher on 2019-07-17.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class UserRoutesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
