112ee374e004115715da981f2d9d6e43bb4e8c29
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-17 14:57:14 -0400

    [Neeraj] Changed UITextfield insert options

---
+++ cityTransitSystemiOS/EditProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditProfileViewController.swift
+//  cityTransitSystemiOS
+//
+//  Created by Neeraj Prasher on 2019-07-17.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //make the navigation bar
+//        let rightButton = UIBarButtonItem(title: "Save", style: UIBarButtonItem.Style.plain, target: self, action: #selector(   self.saveBill))
+//        self.navigationItem.rightBarButtonItem = rightButton
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
