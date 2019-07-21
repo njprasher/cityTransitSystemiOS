a25644eff0bd6c661d08039ebeb33183de630fed
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-20 22:37:42 -0400

    [Neeraj] Added About us screen

---
+++ cityTransitSystemiOS/AboutUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutUsViewController.swift
+//  cityTransitSystemiOS
+//
+//  Created by Neeraj Prasher on 2019-07-20.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
