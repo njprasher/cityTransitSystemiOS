7129526e7b50ece23f6af23f167cf8acb3c17c7a
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-20 23:09:11 -0400

    [Neeraj] Added Contact Us Alert

--- cityTransitSystemiOS/HelpViewController.swift
+++ cityTransitSystemiOS/HelpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class HelpViewController: UIViewController {
 
@@ -16,7 +17,65 @@ class HelpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func btnContactUs(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Contact Us",
+                                      message: "If you need help our email is \n help@routes.com \n +12892330609",
+            preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction = UIAlertAction(title: "Cancel",
+                                     style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        let callAction = UIAlertAction(title: "Call Us",
+                                       style: .default, handler: {
+                                        _ in
+                                        self.makeCall()
+        })
+        alert.addAction(callAction)
+        
+        let smsAction = UIAlertAction(title: "Message Us",
+                                      style: .default, handler: {
+                                        _ in
+                                        self.sendSMS()
+        })
+        alert.addAction(smsAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func makeCall()
+    {
+        if let url = URL(string: "tel://+12892330609)"), UIApplication.shared.canOpenURL(url){
+            if #available(iOS 10, *)
+            {
+                UIApplication.shared.open(url)
+            }
+            else
+            {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+    private func sendSMS()
+    {
+        if MFMessageComposeViewController.canSendText()
+        {
+            let messageVC = MFMessageComposeViewController()
+            
+            messageVC.body = "Hello, How are you?"
+            messageVC.recipients = ["+12892330609"]
+            messageVC.messageComposeDelegate = self as? MFMessageComposeViewControllerDelegate
+            
+            self.present(messageVC, animated: false, completion: nil)
+        }
+        else
+        {
+            print("NO SIM available")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
